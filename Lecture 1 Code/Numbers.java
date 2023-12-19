5b3fe984422297f9e7fe950b665879d830424a16
kristina-tanaskovska <153428660+kristina-tanaskovska@users.noreply.github.com>
2023-12-19 21:17:41 +0100

    Add files via upload

---
+++ Lecture 1 Code/Numbers.java
@@ -0,0 +1,30 @@
+package prog1_en_1;
+import java.util.*;
+public class Numbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x;
+		int y;
+		x = 5;
+		y = 77;
+		System.out.println("X: " + x + ", Y: " + y);
+		System.out.println(x + y);
+		System.out.println("The result is:" + x + y);
+		System.out.println("The result is:" + (x + y));
+		
+		int testNumber = 2000000000;
+		testNumber = testNumber * 2;
+		System.out.println(testNumber);
+	}
+	/* Insert two numbers and print value*/
+	public static void main(Sting[] args){
+		System.out.println("Vnesi two numbers");
+		Scanner sc=new Scanner(System.in);
+		int a= new sc.nextInt();
+		int b= new sc.nextInt();
+		int prod=a*b;
+		System.out.println(a+"x"+b"="+prod);
+	}
+
+}
